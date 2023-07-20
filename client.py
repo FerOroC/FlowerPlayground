0080c5f8eb505f33b58040b60d24bb46e82d1cee
Fermin Orozco <ferorocag@gmail.com>
2023-07-20 22:01:20 +0100

    fix import issue

--- client.py
+++ client.py
@@ -3,23 +3,25 @@ import flwr as fl
 
 class FlowerClient(fl.client.NumPyClient):
     def __init__(self, cid, net, trainloader, valloader):
-        from utils import get_parameters, set_parameters
         self.cid = cid
         self.net = net
         self.trainloader = trainloader
         self.valloader = valloader
 
     def get_parameters(self, config):
+        from utils import get_parameters
         print(f"[Client {self.cid}] get_parameters")
         return get_parameters(self.net)
 
     def fit(self, parameters, config):
+        from utils import get_parameters, set_parameters
         print(f"[Client {self.cid}] fit, config: {config}")
         set_parameters(self.net, parameters)
         train(self.net, self.trainloader, epochs=1)
         return get_parameters(self.net), len(self.trainloader), {}
 
     def evaluate(self, parameters, config):
+        from utils import set_parameters
         print(f"[Client {self.cid}] evaluate, config: {config}")
         set_parameters(self.net, parameters)
         loss, accuracy = test(self.net, self.valloader)
